9dc592197904a665ee081396357931d9df740254
Maxime Bodin <maxime.bodin@mailo.com>
2025-01-09 18:39:44 +0100

    Reformating.

--- todo-lists/src/plugins/level.ts
+++ todo-lists/src/plugins/level.ts
@@ -1,9 +1,9 @@
-import level, {FastifyLeveldbOptions} from "@fastify/leveldb";
+import level, { FastifyLeveldbOptions } from "@fastify/leveldb";
 import fp from "fastify-plugin";
 
 export default fp<FastifyLeveldbOptions>(async (fastify, options) => {
-    fastify.register(level, {
-        path: "./list.db",
-        name: "listsdb"
-    });
-})
+  fastify.register(level, {
+    path: "./list.db",
+    name: "listsdb"
+  });
+});
